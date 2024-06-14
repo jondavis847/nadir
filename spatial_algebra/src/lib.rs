36146b95c26ee1a57b4367ff58d57c3205a95c1a
jondavis847 <jondavis847@gmail.com>
2024-06-14 11:14:13 -0400

    split up spatial value to force and motion, added  impl Add

--- spatial_algebra/src/lib.rs
+++ spatial_algebra/src/lib.rs
@@ -1,14 +1,14 @@
 use linear_algebra::Vector3;
 use rotations::rotation_matrix::RotationMatrix;
-use std::ops::Mul;
+use std::ops::{Add, Mul};
 use transforms::Transform;
 
-pub struct SpatialValue {
+pub struct Motion {
     pub rotation: Vector3,    //TODO should we force this as Cartesian?
     pub translation: Vector3, //TODO should we force this as Cartesian?
 }
 
-impl SpatialValue {
+impl Motion {
     pub fn new(rotation: Vector3, translation: Vector3) -> Self {
         Self {
             rotation,
@@ -17,36 +17,68 @@ impl SpatialValue {
     }
 }
 
-pub enum SpatialVector {
-    Force(SpatialValue),
-    Motion(SpatialValue),
+pub struct Force {
+    pub rotation: Vector3,    //TODO should we force this as Cartesian?
+    pub translation: Vector3, //TODO should we force this as Cartesian?
 }
 
+impl Force {
+    pub fn new(rotation: Vector3, translation: Vector3) -> Self {
+        Self {
+            rotation,
+            translation,
+        }
+    }
+}
 pub struct SpatialTransform {
     value: Transform,
 }
 
-impl Mul<SpatialVector> for SpatialTransform {
-    type Output = SpatialVector;
-    fn mul(self, rhs: SpatialVector) -> SpatialVector {
+impl Add<Motion> for Motion {
+    type Output = Motion;
+    fn add(self, rhs: Motion) -> Motion {
+        Motion::new(
+            self.rotation + rhs.rotation,
+            self.translation + rhs.rotation,
+        )
+    }
+}
+
+impl Mul<Motion> for SpatialTransform {
+    type Output = Motion;
+    fn mul(self, motion: Motion) -> Motion {
         let transform = self.value;
         let rotation_matrix = RotationMatrix::from(transform.rotation).value;
         let r_skew = transform.translation.vec().skew();
-        match rhs {
-            SpatialVector::Motion(motion) => {
-                let rotation = rotation_matrix * motion.rotation;
-                let translation = rotation_matrix * motion.translation
-                    - rotation_matrix * r_skew * motion.rotation;
-                let spatial_value = SpatialValue::new(rotation, translation);
-                SpatialVector::Motion(spatial_value)
-            }
-            SpatialVector::Force(force) => {
-                let rotation =
-                    rotation_matrix * force.rotation - rotation_matrix * r_skew * force.translation;
-                let translation = rotation_matrix * force.translation;
-                let spatial_value = SpatialValue::new(rotation, translation);
-                SpatialVector::Force(spatial_value)
-            }
-        }
+
+        let rotation = rotation_matrix * motion.rotation;
+        let translation =
+            rotation_matrix * motion.translation - rotation_matrix * r_skew * motion.rotation;
+        Motion::new(rotation, translation)
+    }
+}
+
+impl Add<Force> for Force {
+    type Output = Force;
+    fn add(self, rhs: Force) -> Force {
+        Force::new(
+            self.rotation + rhs.rotation,
+            self.translation + rhs.rotation,
+        )
+    }
+}
+
+
+impl Mul<Force> for SpatialTransform {
+    type Output = Force;
+    fn mul(self, force: Force) -> Force {
+        let transform = self.value;
+        let rotation_matrix = RotationMatrix::from(transform.rotation).value;
+        let r_skew = transform.translation.vec().skew();
+
+        let rotation =
+            rotation_matrix * force.rotation - rotation_matrix * r_skew * force.translation;
+        let translation = rotation_matrix * force.translation;
+        Force::new(rotation, translation)
     }
 }
