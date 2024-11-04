29407a4700e585d46a6385c330b8bcd1dabdcd5e
jondavis847 <jondavis847@gmail.com>
2024-11-04 15:52:04 -0500

    cleaning up orbit

--- aerospace/src/orbit.rs
+++ aerospace/src/orbit.rs
@@ -3,18 +3,42 @@ use rotations::{
     prelude::{EulerAngles, EulerSequence},
     Rotation, RotationTrait,
 };
-use std::f64::{consts::PI, INFINITY};
+use std::f64::{consts::PI, INFINITY, NAN};
+use thiserror::Error;
+use time::Time;
+
+/// Tolerance for determining if orbit is elliptical/circular/parabolic/hyperbolic
+const ORBIT_EPSILON: f64 = 1e-6;
+
+#[derive(Error, Debug)]
+pub enum OrbitErrors {
+    #[error("reached max iterations while solving kepler's equation")]
+    KeplerMaxIters,
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum OrbitType {
+    Circular,
+    Elliptical,
+    Parabolic,
+    Hyperbolic,
+}
+
+pub enum Orbit {
+    //Defauney(DelauneyElements), TODO?
+    //Equinoctal(EquinoctalElements), TODO
+    Keplerian(KeplerianElements),
+    //Poincare(PoincareElements), TODO?
+}
 
 /// Represents the orbital elements of a Keplerian orbit.
 ///
 /// This struct contains the orbital parameters necessary to describe an object's orbit
 /// around a central body, based on the Keplerian model. It includes elements such as the
 /// semi-major axis, eccentricity, inclination, RAAN, argument of periapsis, and anomalies.
-pub struct KeplerianOrbit {
-    /// Altitude at the apoapsis based on radius(in km).
-    pub apoapsis_altitude: f64,
-    /// Altitude at the periapsis based on radius(in km).
-    pub periapsis_altitude: f64,
+#[derive(Clone, Copy, Debug)]
+pub struct KeplerianElements {
+    pub epoch: Time,    
     /// Gravitational parameter of the central body (in km^3/s^2).
     pub mu: f64,
     /// Radius of the central body (in km).
@@ -37,11 +61,38 @@ pub struct KeplerianOrbit {
     pub raan: f64,
     /// True anomaly (in radians).
     pub true_anomaly: f64,
+    /// Type of orbit
+    pub orbit_type: OrbitType,
 }
 
-impl KeplerianOrbit {
-    /// Tolernance for determining if orbit is elliptical/circular/parabolic/hyperbolic
-    const ORBIT_EPSILON: f64 = 1e-8;
+impl KeplerianElements {
+    /// Calculates the altitude of the apoapsis (the farthest point from the central body) of the orbit.
+    ///
+    /// # Returns
+    /// - Returns `f64::INFINITY` for parabolic and hyperbolic orbits, as these orbit types have no bounded apoapsis.
+    /// - For other orbit types (e.g., elliptical), returns the altitude of the apoapsis in km, calculated as:
+    ///   `semimajor_axis * (1.0 + eccentricity) - radius`.
+    ///    
+    pub fn get_apoapsis_altitude(&self) -> f64 {
+        match self.orbit_type {
+            OrbitType::Parabolic | OrbitType::Hyperbolic => INFINITY,
+            _ => self.semimajor_axis * (1.0 + self.eccentricity) - self.radius,
+        }
+    }
+
+    /// Calculates the altitude of the periapsis (the closest point to the central body) of the orbit.
+    ///
+    /// # Returns
+    /// - For parabolic and hyperbolic orbits, returns the periapsis altitude as `semiparameter - radius`,
+    ///   because the semi-major axis is considered infinite in these cases, but the periapsis distance is finite.
+    /// - For other orbit types (e.g., elliptical), returns the altitude of the periapsis in km, calculated as:
+    ///   `semimajor_axis * (1.0 - eccentricity) - radius`.    
+    pub fn get_periapsis_altitude(&self) -> f64 {
+        match self.orbit_type {
+            OrbitType::Parabolic | OrbitType::Hyperbolic => self.semiparameter - self.radius, //a is inifinite but p is the periapsis_distance
+            _ => self.semimajor_axis * (1.0 - self.eccentricity) - self.radius,
+        }
+    }
 
     /// Creates a `KeplerianOrbit` instance from position and velocity vectors.
     ///
@@ -53,11 +104,11 @@ impl KeplerianOrbit {
     ///
     /// # Returns
     /// A new `KeplerianOrbit` instance representing the orbit based on the provided position and velocity vectors.
-    pub fn from_rv(mu: f64, radius: f64, r: Vector3<f64>, v: Vector3<f64>) -> Self {
+    pub fn from_rv(mu: f64, radius: f64, r: Vector3<f64>, v: Vector3<f64>, epoch: Time) -> Self {
         // calculate canonical units
         let du = radius;
         let tu = (du.powi(3) / mu).sqrt();
-        // convert to units of body radii
+        // convert to canonical units
         let r = r / du;
         let v = v * tu / du;
         let rm = r.norm();
@@ -71,72 +122,114 @@ impl KeplerianOrbit {
         let n = k.cross(&h);
         let nm = n.norm();
 
-        let e = (vm.powi(2) - 1.0 / rm) * r - rdotv * v;
-        let em = e.norm();
+        let e_vector = (vm.powi(2) - 1.0 / rm) * r - rdotv * v;
+        let e = e_vector.norm();
 
         let zeta = vm.powi(2) / 2.0 - 1.0 / rm;
 
-        let (a, p) = if (em - 1.0).abs() > Self::ORBIT_EPSILON {
-            let a = -1.0 / (2.0 * zeta);
-            let p = a * (1.0 - em.powi(2));
+        // semimajor axis and semilatus rectum (semiparameter)
+        let (a, p) = if (e - 1.0).abs() > ORBIT_EPSILON {
+            let a = -du / (2.0 * zeta);
+            let p = a * (1.0 - e.powi(2));
             (a, p)
         } else {
-            (INFINITY, hm.powi(2))
+            (INFINITY, du * hm.powi(2))
         };
 
+        // inclination
         let i = (h[2] / hm).acos();
-        let raan = if n[1] < 0.0 {
-            2.0 * PI - (n[0] / nm).acos()
+
+        // handles special cases
+        // raan is not defined for equatorial
+        // argp is not defined for circular
+        let is_elliptical_equatorial = e > ORBIT_EPSILON && i < ORBIT_EPSILON;
+        let is_circular_inclined = e < ORBIT_EPSILON && i > ORBIT_EPSILON;
+        let is_circular_equatorial = e < ORBIT_EPSILON && i < ORBIT_EPSILON;
+
+        let i_hat = Vector3::new(1.0, 0.0, 0.0);
+        let (raan, argp, f) = if is_circular_equatorial {
+            let lambda_true = if r[1] < 0.0 {
+                2.0 * PI - (i_hat.dot(&r) / rm).acos()
+            } else {
+                (i_hat.dot(&r) / rm).acos()
+            };
+            (0.0, 0.0, lambda_true)
+        } else if is_circular_inclined {
+            let u = if r[2] < 0.0 {
+                2.0 * PI - (n.dot(&r) / (nm * rm)).acos()
+            } else {
+                n.dot(&r) / (nm * rm).acos()
+            };
+            let raan = if n[1] < 0.0 {
+                2.0 * PI - (n[0] / nm).acos()
+            } else {
+                (n[0] / nm).acos()
+            };
+            (raan, 0.0, u)
+        } else if is_elliptical_equatorial {
+            let w_true = if e_vector[1] < 0.0 {
+                2.0 * PI - (i_hat.dot(&e_vector) / e).acos()
+            } else {
+                (i_hat.dot(&e_vector) / e).acos()
+            };
+
+            let f = if rdotv < 0.0 {
+                2.0 * PI - (e_vector.dot(&r) / (e * rm)).acos()
+            } else {
+                (e_vector.dot(&r) / (e * rm)).acos()
+            };
+
+            (0.0, w_true, f)
         } else {
-            (n[0] / nm).acos()
+            let raan = if n[1] < 0.0 {
+                2.0 * PI - (n[0] / nm).acos()
+            } else {
+                (n[0] / nm).acos()
+            };
+
+            let argp = if e_vector[2] < 0.0 {
+                2.0 * PI - (n.dot(&e_vector) / (nm * e)).acos()
+            } else {
+                (n.dot(&e_vector) / (nm * e)).acos()
+            };
+
+            let f = if rdotv < 0.0 {
+                2.0 * PI - (e_vector.dot(&r) / (e * rm)).acos()
+            } else {
+                (e_vector.dot(&r) / (e * rm)).acos()
+            };
+            (raan, argp, f)
         };
 
-        let argp = if e[2] < 0.0 {
-            2.0 * PI - (n.dot(&e) / (nm * em)).acos()
+        // eccentric and mean anomalies
+        let eccentric_anomaly = (e + (rm * f.cos()) / a).acos(); // Vallado eq 2-8
+        let mean_anomaly = eccentric_anomaly - e * eccentric_anomaly.sin();        
+
+        let orbit_type = if e < ORBIT_EPSILON {
+            OrbitType::Circular
+        } else if e <= 1.0 - ORBIT_EPSILON {
+            OrbitType::Elliptical
+        } else if e < 1.0 + ORBIT_EPSILON {
+            OrbitType::Parabolic
         } else {
-            (n.dot(&e) / (nm * em)).acos()
+            OrbitType::Hyperbolic
         };
 
-        let true_anomaly = if rdotv < 0.0 {
-            2.0 * PI - (e.dot(&r) / (em * rm)).acos()
-        } else {
-            (e.dot(&r) / (em * rm)).acos()
-        };
-
-        // Calculate the mean anomaly (M) from the true anomaly (v) and eccentricity (e)
-        let eccentric_anomaly = 2.0
-            * ((true_anomaly / 2.0).tan())
-                .atan2((1.0 + em).sqrt() * (1.0 - em).sqrt());
-        let mean_anomaly = eccentric_anomaly - em * eccentric_anomaly.sin();        
-
-        let semimajor_axis = a * du;
-
-        // Calculate the periapsis and apoapsis altitudes        
-        let apoapsis_altitude = a * (1.0 + em)  - radius;
-        let periapsis_altitude = if em > 1.0 - Self::ORBIT_EPSILON {
-            //parabolic/hyperbolic, a is inifinite but p is the periapsis_distance
-            p - radius
-        } else {
-            // elliptical/circular
-            a * (1.0 - em) - radius
-        };        
-
-        KeplerianOrbit {            
-            mu,            
-            radius,            
-            semimajor_axis,            
-            semiparameter: p,            
-            argument_of_periapsis: argp,            
-            eccentric_anomaly,            
-            eccentricity: em,            
-            inclination: i,            
-            mean_anomaly,            
+        KeplerianElements {
+            mu,
+            radius,
+            epoch,
+            semimajor_axis: a,
+            semiparameter: p,
+            argument_of_periapsis: argp,
+            eccentric_anomaly,
+            eccentricity: e,
+            inclination: i,
+            mean_anomaly,
             raan,
-            true_anomaly,
-            apoapsis_altitude,
-            periapsis_altitude,
-        }        
-        
+            true_anomaly: f,            
+            orbit_type,
+        }
     }
 
     /// Converts the Keplerian orbital elements into position and velocity vectors.
@@ -174,12 +267,175 @@ impl KeplerianOrbit {
 
         (position, velocity)
     }
+
+    pub fn keplers_problem(&self, new_t: Time) -> Result<Self, OrbitErrors> {
+        let delta_t = new_t - self.epoch;
+
+        let old_anomaly = match self.orbit_type {
+            OrbitType::Circular => {
+                let (r, _) = self.get_rv();
+                let rm = r.norm();
+                let i_hat = Vector3::new(1.0, 0.0, 0.0);
+
+                let lambda_true = if r[1] < 0.0 {
+                    2.0 * PI - (i_hat.dot(&r) / rm).acos()
+                } else {
+                    (i_hat.dot(&r) / rm).acos()
+                };
+                lambda_true
+            }
+            _ => self.nu_to_anomaly(),
+        };
+
+        let new_anomaly = match self.orbit_type {
+            OrbitType::Circular | OrbitType::Elliptical => {
+                let n = (self.mu / (self.semimajor_axis).powi(3)).sqrt(); // mean motion
+                let m0 = old_anomaly - self.eccentricity * old_anomaly.sin(); // initial mean anomaly
+                let mf = m0 + n * delta_t; // final mean anomaly
+                self.keplers_equation_elliptical(mf)
+            }
+            OrbitType::Parabolic => Ok(self.keplers_equation_parabolic(delta_t)),
+            OrbitType::Hyperbolic => {
+                let n = (self.mu / (self.semimajor_axis).powi(3)).sqrt(); // mean motion
+                let m0 = self.eccentricity * old_anomaly.sinh() - old_anomaly; // initial mean anomaly
+                let mf = m0 + n * delta_t; // final mean anomaly
+                self.keplers_equation_hyperbolic(mf)
+            }
+        }?;
+
+        let new_true_anomaly = match self.orbit_type {
+            OrbitType::Circular => new_anomaly,
+            _ => self.anomaly_to_nu(new_anomaly),
+        };
+
+        let mut new_kep = self.clone();
+        new_kep.true_anomaly = new_true_anomaly;
+
+        Ok(new_kep)
+    }
+
+    // Takes mean anomaly (m) as argument and return eccentric anomaly (ea) as output
+    fn keplers_equation_elliptical(&self, ma: f64) -> Result<f64, OrbitErrors> {
+        // calculate the new eccentric anomaly
+        const TOL: f64 = 1e-8;
+        const MAX_ITER: usize = 20;
+        let e = self.eccentricity;
+        let mut ea = if (ma < -PI && ma < 0.0) || ma > PI {
+            ma - e
+        } else {
+            ma + e
+        };
+        let mut residual = 1.0;
+        let mut prev = ea;
+        let mut iter = 0;
+        while residual > TOL {
+            ea = ea + (ma - ea + e * ea.sin()) / (1.0 - e * ea.cos());
+            residual = (ea - prev).abs();
+            prev = ea;
+            iter += 1;
+            if iter > MAX_ITER {
+                return Err(OrbitErrors::KeplerMaxIters);
+            }
+        }
+        Ok(ea)
+    }
+
+    fn keplers_equation_parabolic(&self, dt: f64) -> f64 {
+        // Vallado Algorithm 3
+        // Barker's equation
+        let np = 2.0 * (self.mu / self.semiparameter.powi(3)).sqrt();
+        let s = 0.5 / (1.5 * np * dt).atan();
+        let w = s.tan().cbrt().atan();
+        let b = 2.0 * 1.0 / (2.0 * w).tan();
+        b
+    }
+
+    fn keplers_equation_hyperbolic(&self, ma: f64) -> Result<f64, OrbitErrors> {
+        const TOL: f64 = 1e-8;
+        const MAX_ITER: usize = 20;
+
+        // Vallado Algorithm 4
+        let e = self.eccentricity;
+
+        let mut h = if e < 1.6 {
+            if (-PI < ma && ma < 0.0) || ma > PI {
+                ma - e
+            } else {
+                ma + e
+            }
+        } else {
+            if e < 3.6 && ma.abs() > PI {
+                ma - ma.signum() * e
+            } else {
+                ma / (e - 1.0)
+            }
+        };
+
+        let mut residual = 1.0;
+        let mut prev = h;
+        let mut iter = 0;
+        while residual > TOL {
+            h = h + (ma - h + e * h.sinh()) / (e * h.cosh() - 1.0);
+            residual = (h - prev).abs();
+            prev = h;
+            iter += 1;
+            if iter > MAX_ITER {
+                return Err(OrbitErrors::KeplerMaxIters);
+            }
+        }
+        Ok(h)
+    }
+
+    fn anomaly_to_nu(&self, a: f64) -> f64 {
+        let e = self.eccentricity;
+
+        match self.orbit_type {
+            OrbitType::Circular => NAN,
+            OrbitType::Elliptical => {
+                let c = a.cos();
+                ((c - e) / (1.0 - e * c)).acos()
+            }
+            OrbitType::Parabolic => {
+                // i make the choice to calculate rv here instead of storing the values in
+                // the struct since it's probably very rare that real orbits are parabolic, maybe?
+                // just trying to avoid the overhead of always calculating rv
+                let (r, _) = self.get_rv();
+                let rm = r.norm();
+                (self.semiparameter - rm) / rm
+            }
+            OrbitType::Hyperbolic => {
+                let ch = a.cosh();
+                ((ch - e) / (1.0 - e * ch)).acos()
+            }
+        }
+    }
+
+    fn nu_to_anomaly(&self) -> f64 {
+        // Vallado Algorithm 5
+
+        // converts true anomaly (nu in vallado, f here) and returns:
+        // eccentric anomaly E for elliptical orbits
+        // parabolic anomaly B for parabolic orbits
+        // hyperbolic anomaly H for hyperbolic orbits
+
+        let e = self.eccentricity;
+        let f = self.true_anomaly;
+        let cf = f.cos();
+
+        match self.orbit_type {
+            OrbitType::Circular => NAN, // no perigee
+            OrbitType::Elliptical => ((e + cf) / (1.0 + e * cf)).acos(),
+            OrbitType::Parabolic => (f / 2.0).tan(),
+            OrbitType::Hyperbolic => ((e + cf) / (1.0 + e * cf)).acosh(),
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use approx::assert_abs_diff_eq;
+    use time::TimeSystem;
 
     #[test]
     fn test_from_pv() {
@@ -190,7 +446,9 @@ mod tests {
         let position = Vector3::new(6524.834, 6862.875, 6448.296);
         let velocity = Vector3::new(4.901327, 5.533756, -1.976341);
 
-        let orbit = KeplerianOrbit::from_rv(earth_mu, earth_radius, position, velocity);
+        let epoch = Time::from_ymdhms(2000, 1, 1, 12, 0, 0.0, TimeSystem::UTC).unwrap();
+
+        let orbit = KeplerianElements::from_rv(earth_mu, earth_radius, position, velocity, epoch);
 
         // note that Vallado doesn't include enough decimal places for e-6 accuracy, so we did the float division he provides to get the f64 value
         let expected_semi_major_axis = 36127.343;
@@ -227,8 +485,10 @@ mod tests {
         let position = Vector3::new(6524.834, 6862.875, 6448.296);
         let velocity = Vector3::new(4.901327, 5.533756, -1.976341);
 
+        let epoch = Time::from_ymdhms(2000, 1, 1, 12, 0, 0.0, TimeSystem::UTC).unwrap();
+
         // Create a Keplerian orbit using the from_pv method
-        let orbit = KeplerianOrbit::from_rv(earth_mu, earth_radius, position, velocity);
+        let orbit = KeplerianElements::from_rv(earth_mu, earth_radius, position, velocity, epoch);
 
         // Convert back to position and velocity using get_pv
         let (computed_position, computed_velocity) = orbit.get_rv();
@@ -236,5 +496,42 @@ mod tests {
         // Check that the original and computed values match (within tolerance for floating-point calculations)
         assert_abs_diff_eq!(position, computed_position, epsilon = 1e-2);
         assert_abs_diff_eq!(velocity, computed_velocity, epsilon = 1e-2);
+    }
+
+    #[test]
+    fn test_from_pv_circular() {
+        // values from Vallado
+        let earth_mu = 398600.4415;
+        let earth_radius = 6378.1363;
+
+        let position = Vector3::new(7000.0, 0.0, 0.0);
+        let velocity = Vector3::new(0.0, 0.0, 7.5460533);
+
+        let epoch = Time::from_ymdhms(2000, 1, 1, 12, 0, 0.0, TimeSystem::UTC).unwrap();
+
+        let orbit = KeplerianElements::from_rv(earth_mu, earth_radius, position, velocity, epoch);
+
+        // note that Vallado doesn't include enough decimal places for e-6 accuracy, so we did the float division he provides to get the f64 value
+        let expected_semi_major_axis = 7000.0;
+        let expected_eccentricity = 0.0;
+        let expected_inclination = PI / 2.0;
+        let expected_raan = 0.0;
+        let expected_argument_of_periapsis = 0.0; //float value grabbed from julia satellite toolbox for precision
+        let expected_true_anomaly = 0.0; //float value grabbed from julia satellite toolbox for precision
+
+        assert_abs_diff_eq!(
+            orbit.semimajor_axis,
+            expected_semi_major_axis,
+            epsilon = 1e-2
+        );
+        assert_abs_diff_eq!(orbit.eccentricity, expected_eccentricity, epsilon = 1e-6);
+        assert_abs_diff_eq!(orbit.inclination, expected_inclination, epsilon = 1e-6);
+        assert_abs_diff_eq!(orbit.raan, expected_raan, epsilon = 1e-6);
+        assert_abs_diff_eq!(
+            orbit.argument_of_periapsis,
+            expected_argument_of_periapsis,
+            epsilon = 1e-6
+        );
+        assert_abs_diff_eq!(orbit.true_anomaly, expected_true_anomaly, epsilon = 1e-6);
     }
 }
