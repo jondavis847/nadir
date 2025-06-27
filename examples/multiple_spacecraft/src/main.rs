c09dd35635204050ce08f5e9fdac862eb5a56dff
jpdavis6 <jonathan.p.davis@nasa.gov>
2025-06-27 10:06:54 -0400

    had to reduce tolerances  for multi spacecraft, also had to change software mois to match

--- examples/multiple_spacecraft/src/main.rs
+++ examples/multiple_spacecraft/src/main.rs
@@ -71,11 +71,8 @@ fn main() -> Result<(), Box<dyn Error>> {
         epoch,
         CelestialBodies::Earth,
     );
-    let f1 = FloatingBuilder::new()
-        .with_orbit(orbit1.into())
-        .with_angular_rate(1.0, 0.0, 0.0);
+    let f1 = FloatingBuilder::new().with_orbit(orbit1.into());
 
-    dbg!(&f1);
     let mut j1 = sys.new_joint("f1", f1.into())?;
 
     let mut sc1 = sys.new_body("sc1")?;
@@ -150,89 +147,89 @@ fn main() -> Result<(), Box<dyn Error>> {
     mag1.connect_body(sc1.id, Transform::IDENTITY)?;
 
     // Spacecraft 2
-    // let orbit2 = KeplerianElements::new(
-    //     8e6,
-    //     0.0,
-    //     0.0,
-    //     0.0,
-    //     0.0,
-    //     1e-6,
-    //     epoch,
-    //     CelestialBodies::Earth,
-    // );
-    // let f2 = FloatingBuilder::new().with_orbit(orbit2.into());
-    // let mut j2 = sys.new_joint("f2", f2.into())?;
+    let orbit2 = KeplerianElements::new(
+        8e6,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        1e-6,
+        epoch,
+        CelestialBodies::Earth,
+    );
+    let f2 = FloatingBuilder::new().with_orbit(orbit2.into());
+    let mut j2 = sys.new_joint("f2", f2.into())?;
 
-    // let mut sc2 = sys.new_body("sc2")?;
-    // sc2.set_mass_properties(
-    //     MassPropertiesBuilder::new()
-    //         .with_mass(10.0)?
-    //         .with_ixx(10.0)?
-    //         .with_iyy(10.0)?
-    //         .with_izz(10.0)?,
-    // );
-    // sc2.set_geometry_cuboid(1.0, 0.5, 0.5)?;
-    // sc2.set_material_phong(
-    //     Color::new(0.5, 0.5, 0.5, 1.0),
-    //     32.0,
-    // );
-    // let mut gps2 = SensorBuilder::new(
-    //     "gps2",
-    //     GpsBuilder::new().into(),
-    // );
-    // let mut st2 = SensorBuilder::new(
-    //     "st2",
-    //     StarTrackerBuilder::new().into(),
-    // );
-    // let mut imu2 = SensorBuilder::new(
-    //     "imu2",
-    //     RateGyroBuilder::new().into(),
-    // );
-    // let mut mag2 = SensorBuilder::new(
-    //     "mag2",
-    //     MagnetometerBuilder::new().into(),
-    // );
-    // let mut rw21 = ActuatorBuilder::new(
-    //     "rw21",
-    //     ReactionWheelBuilder::new(0.25, 0.5)?.into(),
-    // );
-    // let mut rw22 = ActuatorBuilder::new(
-    //     "rw22",
-    //     ReactionWheelBuilder::new(0.25, 0.5)?.into(),
-    // );
-    // let mut rw23 = ActuatorBuilder::new(
-    //     "rw23",
-    //     ReactionWheelBuilder::new(0.25, 0.5)?.into(),
-    // );
-    // let rw21_transform = Transform::new(
-    //     Rotation::from(&AlignedAxes::new(
-    //         AxisPair::new(Axis::Zp, Axis::Xp),
-    //         AxisPair::new(Axis::Yp, Axis::Yp),
-    //     )?),
-    //     CoordinateSystem::ZERO,
-    // );
-    // let rw22_transform = Transform::new(
-    //     Rotation::from(&AlignedAxes::new(
-    //         AxisPair::new(Axis::Zp, Axis::Yp),
-    //         AxisPair::new(Axis::Xp, Axis::Xp),
-    //     )?),
-    //     CoordinateSystem::ZERO,
-    // );
-    // let rw23_transform = Transform::new(
-    //     Rotation::IDENTITY,
-    //     CoordinateSystem::ZERO,
-    // );
-    // rw21.connect_body(sc2.id, rw21_transform);
-    // rw22.connect_body(sc2.id, rw22_transform);
-    // rw23.connect_body(sc2.id, rw23_transform);
-    // sys.base
-    //     .connect_outer_joint(&mut j2, Transform::IDENTITY)?;
-    // sc2.connect_inner_joint(&mut j2, Transform::IDENTITY)?;
+    let mut sc2 = sys.new_body("sc2")?;
+    sc2.set_mass_properties(
+        MassPropertiesBuilder::new()
+            .with_mass(10.0)?
+            .with_ixx(10.0)?
+            .with_iyy(10.0)?
+            .with_izz(10.0)?,
+    );
+    sc2.set_geometry_cuboid(1.0, 0.5, 0.5)?;
+    sc2.set_material_phong(
+        Color::new(0.5, 0.5, 0.5, 1.0),
+        32.0,
+    );
+    let mut gps2 = SensorBuilder::new(
+        "gps2",
+        GpsBuilder::new().into(),
+    );
+    let mut st2 = SensorBuilder::new(
+        "st2",
+        StarTrackerBuilder::new().into(),
+    );
+    let mut imu2 = SensorBuilder::new(
+        "imu2",
+        RateGyroBuilder::new().into(),
+    );
+    let mut mag2 = SensorBuilder::new(
+        "mag2",
+        MagnetometerBuilder::new().into(),
+    );
+    let mut rw21 = ActuatorBuilder::new(
+        "rw21",
+        ReactionWheelBuilder::new(0.25, 0.5)?.into(),
+    );
+    let mut rw22 = ActuatorBuilder::new(
+        "rw22",
+        ReactionWheelBuilder::new(0.25, 0.5)?.into(),
+    );
+    let mut rw23 = ActuatorBuilder::new(
+        "rw23",
+        ReactionWheelBuilder::new(0.25, 0.5)?.into(),
+    );
+    let rw21_transform = Transform::new(
+        Rotation::from(&AlignedAxes::new(
+            AxisPair::new(Axis::Zp, Axis::Xp),
+            AxisPair::new(Axis::Yp, Axis::Yp),
+        )?),
+        CoordinateSystem::ZERO,
+    );
+    let rw22_transform = Transform::new(
+        Rotation::from(&AlignedAxes::new(
+            AxisPair::new(Axis::Zp, Axis::Yp),
+            AxisPair::new(Axis::Xp, Axis::Xp),
+        )?),
+        CoordinateSystem::ZERO,
+    );
+    let rw23_transform = Transform::new(
+        Rotation::IDENTITY,
+        CoordinateSystem::ZERO,
+    );
+    rw21.connect_body(sc2.id, rw21_transform);
+    rw22.connect_body(sc2.id, rw22_transform);
+    rw23.connect_body(sc2.id, rw23_transform);
+    sys.base
+        .connect_outer_joint(&mut j2, Transform::IDENTITY)?;
+    sc2.connect_inner_joint(&mut j2, Transform::IDENTITY)?;
 
-    // gps2.connect_body(sc2.id, Transform::IDENTITY)?;
-    // st2.connect_body(sc2.id, Transform::IDENTITY)?;
-    // imu2.connect_body(sc2.id, Transform::IDENTITY)?;
-    // mag2.connect_body(sc2.id, Transform::IDENTITY)?;
+    gps2.connect_body(sc2.id, Transform::IDENTITY)?;
+    st2.connect_body(sc2.id, Transform::IDENTITY)?;
+    imu2.connect_body(sc2.id, Transform::IDENTITY)?;
+    mag2.connect_body(sc2.id, Transform::IDENTITY)?;
 
     sys.add_body(sc1);
     sys.add_joint(j1);
@@ -244,15 +241,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     sys.add_actuator(rw12);
     sys.add_actuator(rw13);
 
-    // sys.add_body(sc2);
-    // sys.add_joint(j2);
-    // sys.add_sensor(gps2);
-    // sys.add_sensor(st2);
-    // sys.add_sensor(imu2);
-    // sys.add_sensor(mag2);
-    // sys.add_actuator(rw21);
-    // sys.add_actuator(rw22);
-    // sys.add_actuator(rw23);
+    sys.add_body(sc2);
+    sys.add_joint(j2);
+    sys.add_sensor(gps2);
+    sys.add_sensor(st2);
+    sys.add_sensor(imu2);
+    sys.add_sensor(mag2);
+    sys.add_actuator(rw21);
+    sys.add_actuator(rw22);
+    sys.add_actuator(rw23);
 
     // Add the software
     let lib_name = match std::env::consts::OS {
@@ -284,34 +281,34 @@ fn main() -> Result<(), Box<dyn Error>> {
         .with_actuator_indices(vec![3, 4, 5])
         .with_sensor_indices(vec![4, 5, 6, 7]);
 
-    // sys.add_software(software1);
-    // sys.add_software(software2);
+    sys.add_software(software1);
+    sys.add_software(software2);
 
     let problem = OdeProblem::new(sys.nominal()?)
-        // .with_periodic_event(PeriodicEvent::new(
-        //     1.0,
-        //     0.0,
-        //     |sys: &mut MultibodySystem, _x: &mut StateVector, _t| {
-        //         sys.software[0]
-        //             .step(
-        //                 &sys.sensors,
-        //                 &mut sys.actuators,
-        //             )
-        //             .unwrap();
-        //     },
-        // ))
-        // .with_periodic_event(PeriodicEvent::new(
-        //     1.0,
-        //     0.0,
-        //     |sys: &mut MultibodySystem, _x: &mut StateVector, _t| {
-        //         sys.software[1]
-        //             .step(
-        //                 &sys.sensors,
-        //                 &mut sys.actuators,
-        //             )
-        //             .unwrap();
-        //     },
-        // ))
+        .with_periodic_event(PeriodicEvent::new(
+            1.0,
+            0.0,
+            |sys: &mut MultibodySystem, _x: &mut StateVector, _t| {
+                sys.software[0]
+                    .step(
+                        &sys.sensors,
+                        &mut sys.actuators,
+                    )
+                    .unwrap();
+            },
+        ))
+        .with_periodic_event(PeriodicEvent::new(
+            1.0,
+            0.0,
+            |sys: &mut MultibodySystem, _x: &mut StateVector, _t| {
+                sys.software[1]
+                    .step(
+                        &sys.sensors,
+                        &mut sys.actuators,
+                    )
+                    .unwrap();
+            },
+        ))
         .with_saving(current_dir()?.join("results"))
         .with_save_event(SaveEvent::new(
             MultibodySystem::init_fn,
@@ -324,7 +321,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     solver.solve_model_adaptive_mut(
         problem,
         (0.0, 1000.0),
-        AdaptiveStepControl::default().with_max_dt(1.0),
+        AdaptiveStepControl::default()
+            .with_abs_tol(1e-9)
+            .with_rel_tol(1e-9),
     )?;
 
     Ok(())
